0289d33be3d83c196255a9513149431881b3388e
Patil, Parikshit <parikshit.patil@siemens.com>
2022-01-29 21:10:45 +0530

    variables added

---
+++ 01-variables/00_declarations.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declare variable
+	var name string
+
+	// assign value to variable
+	name = "Parikshit"
+	fmt.Println("Name:", name)
+
+	// declare & assign value to variable
+	var age int = 25
+
+	// shorhand syntax. compiler will infer
+	village := "Kavathe Ekand"
+
+	fmt.Printf("Age: %d \nVillage: %s\n", age, village)
+
+	// declaring multiple vars
+	var i, j int
+	i = 95
+	j = 56
+	fmt.Printf("%d + %d = %d\n", i, j, i+j)
+}

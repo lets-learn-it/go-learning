f5ab567873fe6b7ff0ecaf53384369d713f40206
Patil, Parikshit <parikshit.patil@siemens.com>
2022-02-20 18:01:03 +0530

    file io

---
+++ 13-io/01-file-io/00_read_using_ioutil.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	data, err := ioutil.ReadFile("demo.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(data))
+}

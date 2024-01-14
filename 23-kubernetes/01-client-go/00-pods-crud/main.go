a93e058c3388b3f4c11a1553b7f485b4c7a030c5
Parikshit Patil <patil8698parikshit@gmail.com>
2024-01-14 12:37:23 +0530

    pod create added

--- 23-kubernetes/01-client-go/00-pods-crud/main.go
+++ 23-kubernetes/01-client-go/00-pods-crud/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 
+	apiv1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -37,6 +38,9 @@ func main() {
 
 	// list all pods which have priority more than 0
 	ListAllPodsFromNamespaceWithField(client, "kube-system", "status.phase=Running")
+
+	// create new pod
+	CreatePodInNamespace(client, "kube-system", "nginx:stable-alpine")
 }
 
 func ListAllPodsFromNamespace(client *kubernetes.Clientset, namespace string) {
@@ -73,4 +77,31 @@ func ListAllPodsFromNamespaceWithField(client *kubernetes.Clientset, namespace s
 	for i, pod := range podList.Items {
 		fmt.Printf("%d: %s, %s\n", i, pod.Name, pod.Status.Phase)
 	}
+}
+
+func CreatePodInNamespace(client *kubernetes.Clientset, namespace string, image string) {
+	pod := apiv1.Pod{
+		ObjectMeta: v1.ObjectMeta{
+			Name: "demo-pod",
+		},
+		Spec: apiv1.PodSpec{
+			Containers: []apiv1.Container{
+				{
+					Image: image,
+					Name:  "nginx",
+					Env: []apiv1.EnvVar{
+						{Name: "demo", Value: "Yes"},
+					},
+				},
+			},
+		},
+	}
+
+	fmt.Printf("Creating pod in %s namespace with image %s\n", namespace, image)
+	newPod, err := client.CoreV1().Pods(namespace).Create(context.TODO(), &pod, v1.CreateOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Printf("New Pod: %s", newPod.Name)
 }
